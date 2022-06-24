ee68bc561687f85af6fed3dc77a6bbfc06d4d3c1
jorfanpabon <jorfanpabon@gmail.com>
2022-06-24 15:54:16 -0500

    Variables .env, new setup app.py & error handler

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, url_for
+from dotenv import load_dotenv
+
+load_dotenv()  # take environment variables from .env
 
 app = Flask(__name__)
 
 
 @app.errorhandler(404)
 def not_found(error):
-    return "<h1>Page not found.</h1>"
+    return render_template('error.html'), 404
 
 
 @app.route('/')
 def index():
-    return "Hello world Flask"
+    return redirect(url_for('home'))
+
+
+@app.route('/home')
+def home():
+    return "Hello world"
+
+
+@app.route('/projects/')
+def projects():
+    return 'The project page'
+
+    
+@app.route('/about')
+def about():
+    return 'The about page'
+
+
+
+
